a2f9ec7fb589785e2cc91ebc48370b6fa5cbfa70
JaimeyHolm <jaimey.holm@student.hu.nl>
2019-02-05 11:14:30 +0100

    Opdrachten

--- Opdrachten/Week_1_Opdracht_1/main.cpp
+++ Opdrachten/Week_1_Opdracht_1/main.cpp
@@ -1,7 +1,30 @@
-#include <stdio.h>
+1. 
+a = 97
+b = 98
+c = 99
+h = 104
+z = 122
 
-int main(int argc, char **argv)
-{
-	printf("hello world\n");
-	return 0;
-}
+2. 
+A = 65
+B = 66
+C = 67
+H = 72
+Z = 90
+
+3. 
+g = 103
+97 - 65 = 32
+103 - 32 = G
+
+4. 
+8 = 56
+0 = 48
+
+5. 
+4 = 52
+0 = 48
+52 - 48 = 4
+
+6. Ze werken hetzelfde als de 'normale' berekeningen
+
---
+++ Opdrachten/Week_1_Opdracht_2/main.cpp
@@ -0,0 +1,23 @@
+A
+
+x = 8
+c = 66
+f = 8
+u = False
+
+p = Foutmelding
+
+station = Foutmelding
+plaats = Foutmelding
+
+y = 3
+z = 3
+t = 3.6
+s = 3.6
+e = 1
+b = True
+
+
+B
+
+Met het "%" teken reken je uit hoeveel (rest) er overblijft als je een getal door een ander getal deelt (10/3 geeft 3 rest 1, dus het antwoord is 1)
