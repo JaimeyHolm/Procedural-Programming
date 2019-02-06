6edc52d2a1084c1386625dfdc2cb16f20cf9a808
JaimeyHolm <43246223+JaimeyHolm@users.noreply.github.com>
2019-02-06 17:13:25 +0100

    Update main.cpp

--- Opdrachten/Week_1_Opdracht_6/main.cpp
+++ Opdrachten/Week_1_Opdracht_6/main.cpp
@@ -4,15 +4,29 @@ using namespace std;
 
 int main(){
 
+	string zin = "";
 	cout << "Geef een string om te vertalen: ";
-	string(zin);
-	cin >> zin;
-	for letter in zin:
-		if letter == e:
-			zin.replace('3')
+	getline(cin, zin);
+	for (int i = 0; i < zin.size(); i++){ 
+		if (zin[i] == 'e'){
+			cout << "3";
+		}
 			
-	
-	cout << zin << "\n" <<
-		"\n";
-
+		else if (zin[i] == 'l'){
+			cout << "1";
+		}
+		
+		else if (zin[i] == 't'){
+			cout << "7";
+		}
+		
+		else if (zin[i] == 'o'){
+			cout << "0";
+		}
+		
+		else {
+			cout << char((zin[i])-32);
+		}
 	}
+	cout << "\n\n\n";
+}
