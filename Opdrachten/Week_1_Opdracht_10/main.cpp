be009dec83f6a110500c56f2af7477d992915c25
JaimeyHolm <43246223+JaimeyHolm@users.noreply.github.com>
2019-02-06 17:20:21 +0100

    Update main.cpp

--- Opdrachten/Week_1_Opdracht_10/main.cpp
+++ Opdrachten/Week_1_Opdracht_10/main.cpp
@@ -13,15 +13,14 @@ int main(){
 		for (int j = 1; j <= i; j++){
 			cout << "*";
 		}
-		
 		cout << "\n";
-		
 	}
+	
 	for(int i = grootte-1; i >= 1; i--){
 		
-        for(int j = 1; j <= i; j++){
-            cout << "*";
-        }
+        	for(int j = 1; j <= i; j++){
+            		cout << "*";
+        	}
 		cout << "\n";
 	}
 	
