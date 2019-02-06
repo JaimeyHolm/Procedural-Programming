3f65fc7873bbba32cc396c1d97a06dcbf7d8e088
JaimeyHolm <43246223+JaimeyHolm@users.noreply.github.com>
2019-02-06 17:08:28 +0100

    Update main.cpp

--- Opdrachten/Week_1_Opdracht_11/main.cpp
+++ Opdrachten/Week_1_Opdracht_11/main.cpp
@@ -1,7 +1,36 @@
-#include <stdio.h>
+//Ik heb voor for-loops gekozen, omdat ik dit het makkelijkst vond werken.
 
-int main(int argc, char **argv)
-{
-	printf("hello world\n");
-	return 0;
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+	cout << "Hoe groot? ";
+	int grootte;
+	cin >> grootte;
+	
+	for (int i = 1; i<=grootte; i++){
+		
+		for (int spaties = 1; spaties <= grootte-i; spaties++){
+			cout << " ";
+		}
+		
+		for (int j = 1; j <= i; j++){
+			cout << "*";
+		}
+		cout << "\n";
+		
+	}
+	for(int i = grootte-1; i >= 1; i--){
+		
+		for (int spaties = 1; spaties <= grootte-i; spaties++){
+			cout << " ";
+		}
+		
+        for(int j = 1; j <= i; j++){
+            cout << "*";
+        }
+		cout << "\n";
+	}
 }
